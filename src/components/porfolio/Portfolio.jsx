56e322735fbab5692c95eca0748711b31522c44d
Joshua <ohisologbosere@gmail.com>
2023-02-23 10:40:11 +0100

    updated code

--- src/components/porfolio/Portfolio.jsx
+++ src/components/porfolio/Portfolio.jsx
@@ -6,14 +6,17 @@ import IMG3 from "../../assests/portfolio3.png";
 import IMG4 from "../../assests/portfolio4.jpeg";
 import IMG5 from "../../assests/portfolio5.JPG";
 import IMG6 from "../../assests/portfolio6.jpg";
+import IMG7 from "../../assests/portfolio7.png";
+import IMG8 from "../../assests/portfolio8.png";
+import IMG9 from "../../assests/portfolio9.jpg";
 
 const data = [
   {
-    id: 2,
-    image: IMG2,
-    title: "twitter Clone",
-    github: "https://github.com/Lord-Constantine/twitter-clone-yt",
-    demo: "https://twitter-clone-ochre-gamma.vercel.app",
+    id: 7,
+    image: IMG7,
+    title: "React-Dashboard",
+    github: "https://github.com/Lord-Constantine/react-dashboard",
+    demo: "https://react-dashboard-7tlumb0xd-lord-constantine.vercel.app/",
   },
   {
     id: 3,
@@ -22,13 +25,7 @@ const data = [
     github: "https://github.com/Lord-Constantine/hulu-clone-yt",
     demo: "https://hulu-clone-yt-drab.vercel.app",
   },
-  {
-    id: 4,
-    image: IMG4,
-    title: "Netflix Clone",
-    github: "https://github.com/Lord-Constantine/Netflix-clone",
-    demo: "https://netflix-clone-8dvnn98q1-lord-constantine.vercel.app/",
-  },
+
   {
     id: 5,
     image: IMG1,
@@ -36,6 +33,30 @@ const data = [
     github: "https://github.com/Lord-Constantine/React-meals",
     demo: "https://react-meals-murex.vercel.app",
   },
+
+  {
+    id: 8,
+    image: IMG8,
+    title: "ChatGPT",
+    github: "https://github.com/Lord-Constantine/gpt3-design",
+    demo: "https://gpt3-design-4ipicrg2x-lord-constantine.vercel.app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "twitter Clone",
+    github: "https://github.com/Lord-Constantine/twitter-clone-yt",
+    demo: "https://twitter-clone-ochre-gamma.vercel.app",
+  },
+
+  {
+    id: 4,
+    image: IMG4,
+    title: "Netflix Clone",
+    github: "https://github.com/Lord-Constantine/Netflix-clone",
+    demo: "https://netflix-clone-8dvnn98q1-lord-constantine.vercel.app/",
+  },
+
   {
     id: 6,
     image: IMG6,
@@ -49,6 +70,13 @@ const data = [
     title: "Facebook Clone",
     github: "https://github.com/Lord-Constantine/facebook-clone",
     demo: "https://facebook-clone-beryl.vercel.app",
+  },
+  {
+    id: 9,
+    image: IMG9,
+    title: "Trillo Landing Page",
+    github: "https://github.com/Lord-Constantine/trillo-project",
+    demo: "https://trillo-project-kohl.vercel.app/",
   },
 ];
 
